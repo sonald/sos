3ace91b54d70117f521ddd90de64af05e3a3e637
Sian Cao <yinshuiboy@gmail.com>
2015-06-28 18:40:02 +0800

    add HashMap and Vector for convience

---
+++ include/map.h
@@ -0,0 +1,121 @@
+#ifndef _SOS_MAP_H
+#define _SOS_MAP_H 
+
+#include <types.h>
+#include <string.h>
+
+using hash_t = uint32_t;
+constexpr int DEFAULT_TABLE_SIZE = 127; // prime
+
+template <class K>  
+struct KeyHash {  
+    hash_t operator()(const K& key) const {
+        return reinterpret_cast<hash_t>(key) % DEFAULT_TABLE_SIZE;
+    }
+};
+
+template <class K>  
+struct KeyEqual {  
+    bool operator()(const K& k1, const K& k2) const {
+        return k1 == k2;
+    }
+};
+
+//BKDR hash
+struct CStringHash {
+    hash_t operator()(const char* key) const {
+        unsigned int seed = 131; // 31 131 13131 1313131 ...
+        hash_t h = 0;
+        while (*key) {
+            h = h * seed + *key++;
+        }
+        return (h & 0x7fffffff);
+    }
+};
+
+struct CStringEqual {  
+    bool operator()(const char* s1, const char* s2) const {
+        return strcmp(s1, s2) == 0;
+    }
+};
+
+template <class K, class V, class F = KeyHash<K>, class Eq = KeyEqual<K>>
+class HashMap 
+{
+    public:
+        HashMap() {
+            memset(_buckets, 0, sizeof _buckets);
+        }
+
+        ~HashMap() {
+            for (int i = 0; i < DEFAULT_TABLE_SIZE; i++) {
+                node_t* p = _buckets[i];
+                while (p) {
+                    auto* d = p;
+                    p = p->next;
+                    delete d;
+                }
+            }
+        }
+
+        bool contains(const K& key) const {
+            auto h = _hfn(key) % DEFAULT_TABLE_SIZE;
+            auto* p = _buckets[h];
+            while (p) {
+                if (_heq(p->k, key)) return true;
+                p = p->next;
+            }
+            return false;
+        }
+
+        V find(const K& key) const {
+            auto h = _hfn(key) % DEFAULT_TABLE_SIZE;
+            auto* p = _buckets[h];
+            while (p) {
+                if (_heq(p->k, key)) return p->v;
+                p = p->next;
+            }
+
+            return V();
+        }
+
+        V erase(const K& key) {
+            auto h = _hfn(key) % DEFAULT_TABLE_SIZE;
+            auto** p = &_buckets[h];
+
+            V ret;
+            while (*p) {
+                if (_heq((*p)->k, key)) {
+                    auto* d = *p;
+                    ret = d->v;
+                    *p = d->next;
+                    delete d;
+                } else
+                    p = &(*p)->next;
+            }
+
+            return ret;
+        }
+
+        void insert(const K& key, const V& val) {
+            auto h = _hfn(key) % DEFAULT_TABLE_SIZE;
+            auto* q = new node_t;
+            q->k = key;
+            q->v = val;
+            q->next = _buckets[h];
+            _buckets[h] = q;
+        }
+
+    private:
+        typedef struct node_ {
+            K k;
+            V v;
+            struct node_* next;
+        } node_t;
+
+        node_t* _buckets[DEFAULT_TABLE_SIZE];
+        F _hfn;
+        Eq _heq;
+};
+
+#endif
---
+++ include/vector.h
@@ -0,0 +1,85 @@
+#ifndef _SOS_VECTOR_H
+#define _SOS_VECTOR_H 
+
+#ifdef __sos__
+#include <common.h>
+#endif
+
+#include <string.h>
+
+template <class T>
+class Vector 
+{
+    public:
+        Vector() {
+            _cap = 16;
+            _sz = 0;
+            _buf = new T[_cap];
+        }
+
+        ~Vector() {
+            delete _buf;
+        }
+
+        void push_back(const T& t) {
+            if (_sz + 1 == _cap) {
+                realloc(_cap*2);
+            }
+
+            _buf[_sz++] = t;
+        }
+
+        T& operator[](int pos) {
+            return _buf[pos];
+        }
+
+        const T& operator[](int pos) const {
+            return _buf[pos];
+        }
+
+        int size() const { return _sz; }
+
+        T remove(int pos) {
+            T t;
+            if (pos >= 0 && pos < _sz) {
+                t = _buf[pos];
+                memmove(_buf+pos, _buf+pos+1, sizeof(T)*(_sz-pos-1));
+                _sz--;
+            }
+            return t;
+        }
+
+        void insert(int pos, const T& t) {
+            if (_sz + 1 >= _cap) realloc(_cap*2);
+            if (pos >= 0 && pos < _sz) {
+                memmove(_buf+pos+1, _buf+pos, sizeof(T)*(_sz-pos));
+                _sz++;
+                _buf[pos] = t;
+            }
+        }
+
+        void pop_back() {
+            if (_sz > 0) --_sz;
+        }
+
+        void clear() {
+            _sz = 0;
+        }
+
+    private:
+        int _cap; 
+        int _sz;
+
+        T* _buf;
+
+        void realloc(int sz) {
+            auto* newp = new T[sz];
+            memcpy(newp, _buf, sizeof(T)*_cap);
+            delete _buf;
+            _buf = newp;
+            _cap = sz;
+        }
+};
+
+#endif
+
